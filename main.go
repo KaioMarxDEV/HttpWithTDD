5baef6cc3e24e0e7cedd4ede48a042bf712a7f6c
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-08 10:02:44 -0300

    feat: first implementation of Store package

--- main.go
+++ main.go
@@ -1,42 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"test/store"
-)
-
 func main() {
-	a := store.Account{}
-	var choice, newName string
 
-	fmt.Println("This is Online Store Program...to manage accounts")
-
-	fmt.Println("Input your first name:")
-	fmt.Scan(&a.FirstName)
-	fmt.Println("Input your last name:")
-	fmt.Scan(&a.LastName)
-
-	fmt.Println("Input where you want to change: [first or last]")
-	fmt.Scan(&choice)
-
-	// verifying is updating first or last name...
-	choicePossible := [2]string{"first", "last"}
-	for _, c := range choicePossible {
-		if c == choice {
-			break
-		}
-
-		fmt.Println("You need to choose just one of the possibles: [first or last]")
-		fmt.Scan(&choice)
-	}
-
-	fmt.Println("What will be your new name: [digits]")
-	fmt.Scan(&newName)
-
-	err := a.ChangeName(choice, newName)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Printf("\naccount data: %v ---- %v", a.FirstName, a.LastName)
 }
--- store/store.go
+++ store/store.go
@@ -1,22 +1,58 @@
 package store
 
-import (
-	"errors"
-)
+import "fmt"
 
+// Account serves as the first abstraction of an user in real world
+// it is base for using methods seen below like Stringer()
 type Account struct {
-	FirstName, LastName string
+	firstName, lastName string
 }
 
-func (a *Account) ChangeName(whichName string, newName string) error {
-	switch {
-	case whichName == "first":
-		a.FirstName = newName
-	case whichName == "last":
-		a.LastName = newName
-	default:
-		return errors.New("name does not support more than first and last")
+// this method is related to objects whom implements account structure
+// and changes the variables saved in Account by parameters passed
+func (a *Account) ChangeName(newFirst string, newLast string) {
+	if newFirst != "" {
+		a.firstName = newFirst
+	}
+	if newLast != "" {
+		a.lastName = newLast
 	}
 
-	return nil
+	fmt.Println("Name changed successfully!")
+}
+
+// Employee is a custom type used to implements everything of Account
+// and also add more variables and methods like seen below its definition:
+type Employee struct {
+	Account
+	numCred float64
+}
+
+// method AddCredits receveis a float and increase the total amount already
+// saved into numCred variable inside the implementation of the receiver (Employee)
+func (e *Employee) AddCredits(amount float64) float64 {
+	e.numCred += amount
+
+	return e.numCred
+}
+
+// method RemoveCredits receveis a float and decrease the total amount already
+// saved into numCred variable inside the implementation of the receiver (Employee)
+func (e *Employee) RemoveCredits(amount float64) float64 {
+	e.numCred -= amount
+
+	return e.numCred
+}
+
+// method CheckCredits is used to verify the total amount in real time inside
+// the variable numCred from an object that implements Employee
+func (e Employee) CheckCredits() float64 {
+	return e.numCred
+}
+
+// method Stringer formats a string with variable values found in Account implementation
+// to use this method the object needs to be an instance of Account struct
+// Employee is an example of Account struct from embedding structs.
+func (e Account) Stringer() string {
+	return fmt.Sprintf("Your Account data, first Name: %v\tlast Name: %v", e.firstName, e.lastName)
 }
