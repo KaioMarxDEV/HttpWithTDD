6f7b972aea3f338a0427ae9725342db13355d812
KaioMarxDEV <kaiomarxdev@gmail.com>
2022-11-09 10:41:57 -0300

    feat: random image generator

--- main.go
+++ main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"image"
+	"image/png"
+	"log"
+	"math/rand"
+	"os"
 )
 
-type MyError struct {
-	when time.Time
-	what string
-}
-
-func (merr *MyError) Error() string {
-	return fmt.Sprintf("When: %v, What: %v", merr.when, merr.what)
-}
-
-func Run() error {
-	return &MyError{when: time.Now(), what: "It didn't work out"}
-}
-
 func main() {
-	if err := Run(); err != nil {
-		fmt.Println(err)
+	rect := image.Rect(0, 0, 100, 100)
+	img := createRandom(rect)
+	save("./newPhoto.png", img)
+}
+
+func save(filePath string, img *image.NRGBA) {
+	imgFile, err := os.Create(filePath)
+	if err != nil {
+		log.Println("cannot save file")
 	}
+	png.Encode(imgFile, img.SubImage(img.Rect))
+	imgFile.Close()
+}
+
+func createRandom(rect image.Rectangle) (created *image.NRGBA) {
+	pix := make([]byte, rect.Dx()*rect.Dy()*4)
+	rand.Read(pix)
+	created = &image.NRGBA{
+		Pix:    pix,
+		Stride: rect.Dx() * 4,
+		Rect:   rect,
+	}
+	return
 }
